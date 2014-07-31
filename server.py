c83baac4cc18a3c4a754993ccaaf34a5175d1763
BorisVerk <boris.verk@gmail.com>
2014-07-31 16:06:36 -0700

    added a python server stub

---
+++ server.py
@@ -0,0 +1,31 @@
+#!flask/bin/python
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+def get_recommendation(user_id):
+    # do we send them the picture
+    return {'beer_id': 123, 'beer_name': 'Asdf Beer', 'beer_description': 'it\'s beer man'}
+
+def add_to_profile(user_id, beer_id, beer_rating):
+    pass
+
+# def log(*args):
+#   for arg in args:
+#     print arg
+
+@app.route('/api/v1/', methods = ['GET'])
+def respond():
+    print request.headers
+    
+    # from request
+    unique_id = 1234 
+    beer_id = 4321
+    beer_rating = 1
+    
+    log(unique_id, beer_id, beer_rating)
+    add_to_profile(unique_id, beer_id, beer_rating)
+    return jsonify( { 'recomendation': get_next_recommendation(unique_id) } )
+
+if __name__ == '__main__':
+    app.run(debug = True)
