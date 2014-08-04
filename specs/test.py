0924821e93c10d2c7cd4cf0d0881c4b9a372ab92
dhfromkorea <dhllee424@gmail.com>
2014-08-04 15:24:26 -0700

    fixed sys path to access server.py functions

--- specs/test.py
+++ specs/test.py
@@ -1,11 +1,11 @@
 # loading depedencies
-import unittest, sys
+import unittest, sys, os
 
 # list of nose.tools can be found at https://nose.readthedocs.org/en/latest/testing_tools.html
 from nose.tools import assert_equal, assert_not_equal
 
 # in order to access objects in server.py
-sys.path.insert(0, '../')
+sys.path.insert(0, os.getcwd())
 import server
 
 class Unit_Test():
@@ -15,18 +15,18 @@ class Unit_Test():
     print "running unit tests for: ", klass
 
   def test_recommend(self):
-    result = server.get_recommendation('something')
+    result = server.get_next_recommendation('something')
     assert_equal(result['beer_id'], 123)
 
   def test_somethingelse(self):
-    result = server.get_recommendation('something')
+    result = server.get_next_recommendation('something')
     assert_not_equal(1234, 123)
 
   def test_somethingelse2(self):
-    result = server.get_recommendation('22222')
+    result = server.get_next_recommendation('22222')
     assert_not_equal(1455, 123)
 
   def test_somethingelse3(self):
-    result = server.get_recommendation('22222')
+    result = server.get_next_recommendation('22222')
     assert_not_equal(dfdsff, 123)
 
