c2ba84127f4911f3e862233f3e8972aae08796f7
dhfromkorea <dhllee424@gmail.com>
2014-08-05 18:29:00 -0700

    fixed yml

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ import os
 urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-print 'database_url', os.environ["DATABASE_URL"]
+print 'database_url', url
 
 db_connection = psycopg2.connect(
    database=url.path[1:],
