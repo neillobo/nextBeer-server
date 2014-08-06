1f870456dbea09c5f78c4e4d6fcd56b3fc0aa9e3
neillobo <neillobo@outlook.com>
2014-08-05 21:30:20 -0700

    fixed issue while running locally

--- server.py
+++ server.py
@@ -13,14 +13,13 @@ app = Flask(__name__)
 # flask url decorators
 
 @app.route('/api/v2/user', methods=['POST'])
-def respond():
+def create_user():
     return "ABD0ACSHT9P2T470B0GJ4930B"
 
 @app.route('/api/v1/<beer_id>', methods = ['GET'])
 @cross_origin()
 def respond(beer_id):
     return jsonify(database.get_next_recommendation(beer_id))
-
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
