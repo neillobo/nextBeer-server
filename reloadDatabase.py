8999b7531344f54636e0bbcc12776ce62dec1e48
BorisVerk <boris.verk@gmail.com>
2014-08-06 12:10:06 -0700

    modified databse reloading script to reflect changes in files

--- reloadDatabase.py
+++ reloadDatabase.py
@@ -6,8 +6,8 @@ import urlparse
 from postgres import Postgres
 import os
 
-distances_file_name  = "./data/distances100.csv"
-beer_names_file_name = "./data/beernames100.csv"
+distances_file_name  = "./data/distances.csv"
+beer_names_file_name = "./data/metadata.csv"
 
 db_location = os.environ.get("DATABASE_URL", "postgres://craig:helloworld@127.0.0.1:5432/testdb")
 db = Postgres(db_location)
@@ -17,15 +17,16 @@ try:
 except:
     pass
 
-db.run("CREATE TABLE beer_names(beer_id int PRIMARY KEY NOT NULL, beer_name varchar)")
+db.run("CREATE TABLE beer_names(beer_id int PRIMARY KEY NOT NULL, beer_name varchar, beer_image_url varchar)")
 with open (beer_names_file_name,"r") as infile:
     for line in infile:
         comma_seperated_values = line.strip().split(',')
         values = {
             "beer_id" : comma_seperated_values[0],
-            "beer_name" : comma_seperated_values[1]
+            "beer_name" : comma_seperated_values[1],
+            "beer_image_url": comma_seperated_values[1]
         }
-        db.run("INSERT INTO beer_names VALUES(%(beer_id)s,%(beer_name)s)", values)
+        db.run("INSERT INTO beer_names VALUES(%(beer_id)s,%(beer_name)s,%(beer_image_url)s)", values)
 
 try:
     db.run("DROP TABLE distances")
