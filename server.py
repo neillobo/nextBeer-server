b203b0d40d38b39803c36875608915194d4cbcfb
neillobo <neillobo@outlook.com>
2014-08-13 21:41:24 -0700

    testing for cors, commenting out code

--- server.py
+++ server.py
@@ -43,10 +43,11 @@ def get_next_recommendation():
     data = request.json
     beer_id = data['beer_id']
     beer_rating = data['beer_rating']
-    user_id = database.get_userid_from_string(token)
-    database.save_to_profile(user_id, beer_id, beer_rating)
-    recommended_beer_id = database.get_next_recommendation(user_id)
-    return jsonify(database.get_metadata(recommended_beer_id))
+    return token
+    # user_id = database.get_userid_from_string(token)
+    # database.save_to_profile(user_id, beer_id, beer_rating)
+    # recommended_beer_id = database.get_next_recommendation(user_id)
+    # return jsonify(database.get_metadata(recommended_beer_id))
 
 @app.route('/api/v1/<beer_id>', methods = ['GET'])
 def get_similar_beer(beer_id):
