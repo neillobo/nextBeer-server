b18ce9a7675edf5a05d28ef004d362f764d6ed5b
BorisVerk <boris.verk@gmail.com>
2014-08-22 12:54:49 -0700

    removed v2 of api

--- server.py
+++ server.py
@@ -13,11 +13,18 @@ app.config['CORS_HEADERS'] = ['Content-Type','Authorization']
 app.config['CORS_RESOURCES'] = {'/api/*': {'origins': '*'}}
 CORS(app)
 
-seed_list = list('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ') # string.digits + string.uppercase
 @app.route('/api/v3/user', methods=['POST'])
+@app.route('/api/v2/user', methods=['POST'])
 def create_new_user():
-    # generate a "random" (36**10 possibilities) identifier for a user
-    unique_string = "".join([random.choice(seed_list) for _ in range(10)])
+    '''
+    Generate a random (36**10 possibilities) identifier for the client
+    This request is made when the app starts the very first time.
+    The client then uses this unique_string as an identifier in all subsequent
+    requests it makes.
+    The unique_string can be thought of as a randomly generated username.
+    '''
+    possible_chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ' # string.digits + string.uppercase
+    unique_string = ''.join([random.choice(possible_chars) for _ in range(10)])
 
     database.save_new_user(unique_string)
     return jsonify({"token": unique_string})
@@ -25,22 +32,6 @@ def create_new_user():
 # API v3 uses the Weighted Slope One Algorithm
 @app.route('/api/v3/rate', methods = ['POST'])
 def get_best_recommendation():
-    unique_string = request.headers['Authorization']
-    data = request.json
-    beer_id = data['beer_id']
-    beer_rating = data['beer_rating']
-
-    user_id = database.get_userid_from_string(unique_string)
-    database.save_to_profile(user_id, beer_id, beer_rating)
-
-    recommended_beer_id = database.get_best_recommendation(user_id)
-    database.save_recommendation(user_id,recommended_beer_id) # save the recommendation so we dont return it again
-    print recommended_beer_id
-    return jsonify(database.get_metadata(recommended_beer_id))
-
-
-@app.route('/api/v2/rate', methods = ['POST'])
-def get_next_recommendation():
     '''
     The client sends a POST requests that represents a review of a beer we
     recommended previously. We save this rating and send back a new
@@ -52,7 +43,7 @@ def get_next_recommendation():
     try:
         unique_string = request.headers['Authorization']
     except KeyError:
-        print 'sent review without auth'
+        print 'client sent review without auth'
         return jsonify({}), 401
 
     user_id = database.get_userid_from_string(unique_string)
@@ -79,10 +70,7 @@ def get_next_recommendation():
     database.save_to_profile(user_id, beer_id, beer_rating)
 
     recommended_beer_id = database.get_next_recommendation(user_id)
-
-    # mark that this beer has been recommended
-    database.save_to_profile(user_id, recommended_beer_id, 0)
-
+    database.save_recommendation(user_id,recommended_beer_id) # save the recommendation so we dont return it again
     return jsonify(database.get_metadata(recommended_beer_id))
 
 
