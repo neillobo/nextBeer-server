d361a9efe45b551418ae469d7c1ae603a0daeeee
neillobo <neillobo@outlook.com>
2014-08-22 20:49:07 -0700

    added debug=true

--- server.py
+++ server.py
@@ -93,4 +93,4 @@ def get_similar_beer(beer_id):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
